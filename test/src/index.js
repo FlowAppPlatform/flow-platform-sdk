44f1c7f653192d9db5499a5575df02c0ce592b97
Aditya Agarwal <adityaa803@gmail.com>
2018-09-10 18:38:16 +0530

    fixes no-unused vars issue

--- test/src/index.js
+++ test/src/index.js
@@ -1,19 +1,14 @@
+/* eslint-disable no-unused-vars */
+
 import Flow from '../../src/index'
 import Graph from './Graph'
 import Variable from './Variable'
 import Component from './Component'
 import Port from './Port'
 
-var Tests = {
-  Component,
-  Port,
-  Variable,
-  Graph
-}
-
 describe('should isServer & isClient works', function () {
   it('isServer returns true for node env', function (done) {
-    global.window = undefined;
+    global.window = undefined
     const runningOnServer = Flow.isServer()
     if (runningOnServer) {
       done()
@@ -23,7 +18,7 @@ describe('should isServer & isClient works', function () {
   })
 
   it('isServer returns false for browser env', function (done) {
-    global.window = {};
+    global.window = {}
     const runningOnServer = Flow.isServer()
     if (!runningOnServer) {
       done()
@@ -33,7 +28,7 @@ describe('should isServer & isClient works', function () {
   })
 
   it('isClient returns false for node env', function (done) {
-    global.window = undefined;
+    global.window = undefined
     const runningOnServer = Flow.isClient()
     if (!runningOnServer) {
       done()
@@ -43,7 +38,7 @@ describe('should isServer & isClient works', function () {
   })
 
   it('isClient returns true for browser env', function (done) {
-    global.window = {};
+    global.window = {}
     const runningOnServer = Flow.isClient()
     if (runningOnServer) {
       done()
