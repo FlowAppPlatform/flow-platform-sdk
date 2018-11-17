bb37f92daf4dcc79aae04da88a0703897a090c88
Mohammed Faizuddin <mdfaizuddin15@gmail.com>
2018-11-17 19:48:44 +0530

    dynamic properties only when isOnGraph()
    more linting

--- src/Component.js
+++ src/Component.js
@@ -2,87 +2,87 @@
  * Documentation at /docs/classes/Component/README.md
  */
 
-import Util from "./Util";
-import Port from "./Port";
-import Property from "./Property";
+import Util from './Util'
+import Port from './Port'
+import Property from './Property'
 
 class Component {
-  constructor(id) {
+  constructor (id) {
     // Icon URL is the URL of an Icon in SVG that can be showed in the UI.
-    this._iconUrl = "";
-    this._type = "component";
+    this._iconUrl = ''
+    this._type = 'component'
 
     // These are number of outports.
-    this._ports = [];
+    this._ports = []
 
     // A short description of the component.
-    this._description = "";
+    this._description = ''
 
     // A socket object to communicate with other components.
-    this._socket = null;
+    this._socket = null
 
     // Genere a new ID for this instance.
-    this._id = id || Util.generateId();
+    this._id = id || Util.generateId()
 
-    this._task = null;
+    this._task = null
 
-    this._propertys = [];
+    this._propertys = []
 
     // check if the env is NodeJS
-    this._platform = "browser";
+    this._platform = 'browser'
     if (
-      typeof process !== "undefined" &&
+      typeof process !== 'undefined' &&
       process.versions &&
       process.versions.node
     ) {
-      this._platform = "node";
+      this._platform = 'node'
     }
 
-    this._isProcessingTask = false;
+    this._isProcessingTask = false
 
     // The execution platform
-    this._executionPlatform = [];
+    this._executionPlatform = []
   }
 
-  isOnGraph() {
+  isOnGraph () {
     // The Socket belongs to a Graph. If this component has a socket. It's on the graph.
     if (this._socket) {
-      return true;
+      return true
     } else {
-      return false;
+      return false
     }
   }
 
-  initProperties(properties) {
+  initProperties (properties) {
     for (var key in properties) {
-      if (this.hasProperty(key)) this.getProperty(key).data = properties[key];
+      if (this.hasProperty(key)) this.getProperty(key).data = properties[key]
     }
   }
 
-  initConnections(connections) {
+  initConnections (connections) {
     for (var i = 0; i < connections.length; i++) {
       this.getPort(connections[i].fromPortId).connectComponentById(
         connections[i].toComponentId
-      );
+      )
     }
   }
 
-  addProperties(properties) {
+  addProperties (properties) {
     if (properties instanceof Property) {
-      this.addProperty(properties);
+      this.addProperty(properties)
     } else if (properties instanceof Array) {
       for (let i = 0; i < properties.length; i++) {
-        this.addProperty(properties[i]);
+        this.addProperty(properties[i])
       }
     } else {
-      throw new Error("propertys should be an instance of Property class.");
+      throw new Error('propertys should be an instance of Property class.')
     }
   }
 
-  addProperty(property) {
+  addProperty (property) {
     if (property instanceof Property) {
       if (!property.id) {
-        throw new Error("Property does not have an ID.");
+        throw new Error('Property does not have an ID.')
       }
 
       for (let i = 0; i < this._propertys.length; i++) {
@@ -90,45 +90,45 @@ class Component {
           property.name === this._propertys[i].name ||
           property.id === this._propertys[i].id
         ) {
-          throw new Error("Property with the same name or id already exists.");
+          throw new Error('Property with the same name or id already exists.')
         }
       }
 
-      this._propertys.push(property);
+      this._propertys.push(property)
     } else {
-      throw new Error("propertys should be an instance of Property class.");
+      throw new Error('propertys should be an instance of Property class.')
     }
   }
 
-  removeProperty(property) {
-    if (property instanceof Property || typeof property === "string") {
+  removeProperty (property) {
+    if (property instanceof Property || typeof property === 'string') {
       if (property instanceof Property && !property.id) {
-        throw new Error("Property does not have an ID.");
+        throw new Error('Property does not have an ID.')
       }
 
       for (let i = 0; i < this._propertys.length; i++) {
-        if (typeof property === "string") {
+        if (typeof property === 'string') {
           if (property === this._propertys[i].name) {
-            this._propertys.splice(i, 1);
+            this._propertys.splice(i, 1)
           }
         } else {
           if (
             property.name === this._propertys[i].name ||
             property.id === this._propertys[i].id
           ) {
-            this._propertys.splice(i, 1);
+            this._propertys.splice(i, 1)
           }
         }
       }
     } else {
-      throw new Error("propertys should be an instance of Property class.");
+      throw new Error('propertys should be an instance of Property class.')
     }
   }
 
-  updateProperty(property) {
+  updateProperty (property) {
     if (property instanceof Property) {
       if (!property.id) {
-        throw new Error("Property does not have an ID.");
+        throw new Error('Property does not have an ID.')
       }
 
       for (let i = 0; i < this._propertys.length; i++) {
@@ -136,119 +136,121 @@ class Component {
           property.name === this._propertys[i].name ||
           property.id === this._propertys[i].id
         ) {
-          this._propertys[i] = property;
-          return;
+          this._propertys[i] = property
+          return
         }
       }
 
-      throw new Error("Property not found and is not updated.");
+      throw new Error('Property not found and is not updated.')
     } else {
-      throw new Error("propertys should be an instance of Property class.");
+      throw new Error('propertys should be an instance of Property class.')
     }
   }
 
-  getProperty(property) {
-    if (property instanceof Property || typeof property === "string") {
+  getProperty (property) {
+    if (property instanceof Property || typeof property === 'string') {
       if (property instanceof Property && !property.id) {
-        throw new Error("Property does not have an ID.");
+        throw new Error('Property does not have an ID.')
       }
 
       for (let i = 0; i < this._propertys.length; i++) {
-        if (typeof property === "string") {
+        if (typeof property === 'string') {
           if (property === this._propertys[i].name) {
-            return this._propertys[i];
+            return this._propertys[i]
           }
         } else {
           if (
             property.name === this._propertys[i].name ||
             property.id === this._propertys[i].id
           ) {
-            return this._propertys[i];
+            return this._propertys[i]
           }
         }
       }
 
-      throw new Error("Property not found.");
+      throw new Error('Property not found.')
     } else {
-      throw new Error("Property should be an instance of property class.");
+      throw new Error('Property should be an instance of property class.')
     }
   }
 
-  hasProperty(property) {
-    if (property instanceof Property || typeof property === "string") {
+  hasProperty (property) {
+    if (property instanceof Property || typeof property === 'string') {
       if (property instanceof Property && !property.id) {
-        throw new Error("Property does not have an ID.");
+        throw new Error('Property does not have an ID.')
       }
 
       for (let i = 0; i < this._propertys.length; i++) {
-        if (typeof property === "string") {
+        if (typeof property === 'string') {
           if (property === this._propertys[i].name) {
-            return true;
+            return true
           }
         } else {
           if (
             property.name === this._propertys[i].name ||
             property.id === this._propertys[i].id
           ) {
-            return true;
+            return true
           }
         }
       }
-      return false;
+      return false
     } else {
-      throw new Error("propertys should be an instance of Property class.");
+      throw new Error('propertys should be an instance of Property class.')
     }
   }
 
   // execute the component task.
-  execute() {
-    var component = this;
+  execute () {
+    var component = this
 
     if (
       arguments[0] &&
       arguments[0]._type &&
-      arguments[0]._type === "component"
+      arguments[0]._type === 'component'
     ) {
-      component = arguments[0];
+      component = arguments[0]
     }
 
     if (!component._task) {
-      throw new Error("No task attached.");
+      throw new Error('No task attached.')
     }
     // check if task is attached and if its actually a function.
     if (component._isProcessingTask) {
-      throw new Error("Component is already processing a task.");
+      throw new Error('Component is already processing a task.')
     }
 
-    if (component._task && Util.validateType(component._task, "function")) {
-      component._isProcessingTask = true;
-      component._socket.emit(`resolve-properties-${component.id}`);
-      component._task();
+    if (component._task && Util.validateType(component._task, 'function')) {
+      component._isProcessingTask = true
+      if (this.isOnGraph()) {
+        component._socket.emit(`resolve-properties-${component.id}`)
+      }
+      component._task()
     }
   }
 
-  taskComplete() {
+  taskComplete () {
     // Task is complete, and this component is no longer processing.
-    this._isProcessingTask = false;
+    this._isProcessingTask = false
   }
   // Task is a custom code as a function that runs when the component executes.
-  attachTask(task) {
-    if (!Util.validateType(task, "function")) {
-      throw new Error("Task must be a function.");
+  attachTask (task) {
+    if (!Util.validateType(task, 'function')) {
+      throw new Error('Task must be a function.')
     }
 
-    this._task = task;
+    this._task = task
   }
 
   // Task is a custom code as a function that runs when the component executes.
-  detachTask() {
-    this._task = null;
+  detachTask () {
+    this._task = null
   }
 
-  addPort(port) {
+  addPort (port) {
     if (port instanceof Port) {
       if (!port.id) {
-        throw new Error("Port does not have an ID.");
+        throw new Error('Port does not have an ID.')
       }
 
       for (let i = 0; i < this._ports.length; i++) {
@@ -256,183 +258,183 @@ class Component {
           port.name === this._ports[i].name ||
           port.id === this._ports[i].id
         ) {
-          throw new Error("Port with the same name or id already exists.");
+          throw new Error('Port with the same name or id already exists.')
         }
       }
 
       if (port._componentAttachedTo) {
-        throw new Error("This port is already attached with other component");
+        throw new Error('This port is already attached with other component')
       }
 
-      port._componentAttachedTo = this;
+      port._componentAttachedTo = this
 
-      this._ports.push(port);
+      this._ports.push(port)
     } else {
-      throw new Error("Port should be an instance of Port class.");
+      throw new Error('Port should be an instance of Port class.')
     }
   }
 
-  removePort(port) {
+  removePort (port) {
     if (port instanceof Port) {
       if (!port.id) {
-        throw new Error("Port does not have an ID.");
+        throw new Error('Port does not have an ID.')
       }
       if (this._ports.indexOf(port) < 0) {
-        throw new Error("Port not found in the component.");
+        throw new Error('Port not found in the component.')
       }
-      this._ports.splice(this._ports.indexOf(port), 1);
-      port._componentAttachedTo = null;
+      this._ports.splice(this._ports.indexOf(port), 1)
+      port._componentAttachedTo = null
     } else {
-      throw new Error("Port should be an instance of Port class.");
+      throw new Error('Port should be an instance of Port class.')
     }
   }
 
-  getPort(port) {
-    if (port instanceof Port || typeof port === "string") {
+  getPort (port) {
+    if (port instanceof Port || typeof port === 'string') {
       if (port instanceof Port) {
         if (!port.id) {
-          throw new Error("Port does not have an ID.");
+          throw new Error('Port does not have an ID.')
         }
 
         if (this._ports.indexOf(port) < 0) {
-          throw new Error("Port not found in the component.");
+          throw new Error('Port not found in the component.')
         }
 
-        return this._ports[this._ports.indexOf(port)];
+        return this._ports[this._ports.indexOf(port)]
       }
 
-      if (typeof port === "string") {
+      if (typeof port === 'string') {
         for (var i = 0; i < this._ports.length; i++) {
           if (this._ports[i].name === port) {
-            return this._ports[i];
+            return this._ports[i]
           }
         }
 
         throw new Error(
-          "Port with name " + port + ` not found in the component ${this.id}`
-        );
+          'Port with name ' + port + ` not found in the component ${this.id}`
+        )
       }
     } else {
-      throw new Error("Port should be an instance of Port class.");
+      throw new Error('Port should be an instance of Port class.')
     }
   }
 
-  hasPort(port) {
+  hasPort (port) {
     if (port instanceof Port) {
       if (!port.id) {
-        throw new Error("Port does not have an ID.");
+        throw new Error('Port does not have an ID.')
       }
       if (this._ports.indexOf(port) < 0) {
-        return false;
+        return false
       }
 
-      return true;
+      return true
     } else {
-      throw new Error("Port should be an instance of Port class.");
+      throw new Error('Port should be an instance of Port class.')
     }
   }
 
-  getPorts() {
-    return this._ports;
+  getPorts () {
+    return this._ports
   }
 
   // Private Functions.
 
-  _attachSocket(socket) {
-    this._socket = socket;
-    var component = this;
-    this._socket.on("execute-component-" + this.id, function() {
-      component.execute(component);
-    });
+  _attachSocket (socket) {
+    this._socket = socket
+    var component = this
+    this._socket.on('execute-component-' + this.id, function () {
+      component.execute(component)
+    })
   }
 
-  _detachSocket() {
+  _detachSocket () {
     if (this._socket) {
-      this._socket.off("execute-component-" + this.id, this.execute);
-      this._socket = null;
+      this._socket.off('execute-component-' + this.id, this.execute)
+      this._socket = null
     }
   }
 
-  setExecutionPlatform(platform) {
+  setExecutionPlatform (platform) {
     if (Array.isArray(platform)) {
       if (platform.length > 2) {
-        throw new Error("Too many elements in the array");
+        throw new Error('Too many elements in the array')
       }
 
       platform.forEach(arg => {
-        if (arg !== "server" && arg !== "client") {
-          throw new Error("Invalid argument");
+        if (arg !== 'server' && arg !== 'client') {
+          throw new Error('Invalid argument')
         }
-      });
-      this._executionPlatform = platform;
+      })
+      this._executionPlatform = platform
     } else {
-      if (typeof platform !== "string") {
-        throw new Error("Platform should be a string");
+      if (typeof platform !== 'string') {
+        throw new Error('Platform should be a string')
       }
 
-      if (platform !== "server" && platform !== "client") {
-        throw new Error("Invalid string");
+      if (platform !== 'server' && platform !== 'client') {
+        throw new Error('Invalid string')
       }
 
-      this._executionPlatform = [platform];
+      this._executionPlatform = [platform]
     }
   }
 
   // getters and setters
-  get description() {
-    return this._description;
+  get description () {
+    return this._description
   }
 
-  set description(description) {
-    if (!Util.validateType(description, "string")) {
-      throw new Error("Description must be a string.");
+  set description (description) {
+    if (!Util.validateType(description, 'string')) {
+      throw new Error('Description must be a string.')
     }
 
-    this._description = description;
+    this._description = description
   }
 
-  get task() {
-    return this._task;
+  get task () {
+    return this._task
   }
 
-  set task(task) {
-    this.attachTask(task);
+  set task (task) {
+    this.attachTask(task)
   }
 
-  get name() {
-    return this._name;
+  get name () {
+    return this._name
   }
 
-  set name(name) {
-    if (!Util.validateType(name, "string")) {
-      throw new Error("Name must be a string.");
+  set name (name) {
+    if (!Util.validateType(name, 'string')) {
+      throw new Error('Name must be a string.')
     }
-    this._name = name;
+    this._name = name
   }
 
-  get iconUrl() {
-    return this._iconUrl;
+  get iconUrl () {
+    return this._iconUrl
   }
 
-  set iconUrl(iconUrl) {
-    if (!Util.validateType(iconUrl, "url")) {
-      throw new Error("iconUrl must be a URL.");
+  set iconUrl (iconUrl) {
+    if (!Util.validateType(iconUrl, 'url')) {
+      throw new Error('iconUrl must be a URL.')
     }
-    this._iconUrl = iconUrl;
+    this._iconUrl = iconUrl
   }
 
-  get ports() {
-    return this._ports;
+  get ports () {
+    return this._ports
   }
 
-  get id() {
-    return this._id;
+  get id () {
+    return this._id
   }
 
-  set id(id) {
-    throw new Error("ID is read-only");
+  set id (id) {
+    throw new Error('ID is read-only')
   }
 }
 
 // export.
-module.exports = Component;
+module.exports = Component
--- src/Graph.js
+++ src/Graph.js
@@ -2,63 +2,63 @@
  * Documentation at /docs/classes/Graph/README.md
  */
 
-import EventEmitter from "event-emitter";
-import Util from "./Util";
+import EventEmitter from 'event-emitter'
+import Util from './Util'
 
 class Graph {
-  constructor(name) {
-    this._socket = new EventEmitter();
-    this._type = "graph";
-    this.name = name;
+  constructor (name) {
+    this._socket = new EventEmitter()
+    this._type = 'graph'
+    this.name = name
     // Genere a new ID for this instance.
-    this._id = Util.generateId();
-    this._components = {};
+    this._id = Util.generateId()
+    this._components = {}
   }
 
-  addComponent(component) {
+  addComponent (component) {
     if (
-      typeof component === "object" &&
+      typeof component === 'object' &&
       component._type &&
-      component._type === "component"
+      component._type === 'component'
     ) {
       if (!component.id) {
         throw new Error(
-          "Component does not have an id. Please instantiate the component."
-        );
+          'Component does not have an id. Please instantiate the component.'
+        )
       }
 
-      this._components[component.id] = component;
-      component._attachSocket(this._socket);
+      this._components[component.id] = component
+      component._attachSocket(this._socket)
       this._socket.on(`resolve-properties-${component.id}`, () => {
-        this.resolveDynamicProperties(component.id);
-      });
+        this.resolveDynamicProperties(component.id)
+      })
     } else {
-      throw new Error("Argument 1 is not of type Component");
+      throw new Error('Argument 1 is not of type Component')
     }
   }
 
-  removeComponent(component) {
+  removeComponent (component) {
     if (
-      typeof component === "object" &&
+      typeof component === 'object' &&
       component._type &&
-      component._type === "component"
+      component._type === 'component'
     ) {
       if (!component.id) {
         throw new Error(
-          "Component does not have an id. Please instantiate the component."
-        );
+          'Component does not have an id. Please instantiate the component.'
+        )
       }
 
-      delete this._components[component.id];
-      component._detachSocket();
+      delete this._components[component.id]
+      component._detachSocket()
     } else {
-      throw new Error("Argument 1 is not of type Component");
+      throw new Error('Argument 1 is not of type Component')
     }
   }
 
-  init(graphJson, componentClasses) {
+  init (graphJson, componentClasses) {
     if (graphJson) {
-      this.name = graphJson.name;
+      this.name = graphJson.name
       for (var i = 0; i < graphJson.data.length; i++) {
         if (
           graphJson.data[i].graphComponentId &&
@@ -66,91 +66,91 @@ class Graph {
         ) {
           let component = new componentClasses[
             graphJson.data[i].graphComponentId
-          ](graphJson.data[i].id);
+          ](graphJson.data[i].id)
           // add property data
-          component.initProperties(graphJson.data[i].propertyData);
+          component.initProperties(graphJson.data[i].propertyData)
           // add connections.
-          component.initConnections(graphJson.data[i].connections);
-          this.addComponent(component);
+          component.initConnections(graphJson.data[i].connections)
+          this.addComponent(component)
         }
       }
     } else {
-      throw new Error("Graph JSON is not a valid JSON object");
+      throw new Error('Graph JSON is not a valid JSON object')
     }
   }
 
-  execute() {
+  execute () {
     // execute the start component of a graph.
-    if (this._components["start"]) {
-      this._components["start"].execute();
+    if (this._components['start']) {
+      this._components['start'].execute()
     }
   }
 
-  resolveDynamicProperties(targetComponentId) {
-    var targetComponent = this._components[targetComponentId];
+  resolveDynamicProperties (targetComponentId) {
+    var targetComponent = this._components[targetComponentId]
     targetComponent._propertys.forEach(property => {
-      const resolved = resolvePath(property.data);
-      if (typeof resolved !== "string") {
-        const { componentId: sourceComponentId, portId, propertyId } = resolved;
+      const resolved = resolvePath(property.data)
+      if (typeof resolved !== 'string') {
+        const { componentId: sourceComponentId, portId, propertyId } = resolved
         if (sourceComponentId && portId && propertyId) {
-          const sourceComponent = this._components[sourceComponentId];
+          const sourceComponent = this._components[sourceComponentId]
           const value = sourceComponent.getPort(portId).getProperty(propertyId)
-            .data;
+            .data
           if (value) {
-            property.data = value;
+            property.data = value
           }
         }
       }
-    });
+    })
   }
 
   // getters and setters.
-  get name() {
-    return this._name;
+  get name () {
+    return this._name
   }
 
-  set name(name) {
-    if (!Util.validateType(name, "string")) {
-      throw new Error("Name must be a string.");
+  set name (name) {
+    if (!Util.validateType(name, 'string')) {
+      throw new Error('Name must be a string.')
     }
-    this._name = name;
+    this._name = name
   }
 
-  get id() {
-    return this._id;
+  get id () {
+    return this._id
   }
 
-  set id(id) {
-    throw new Error("ID is read-only");
+  set id (id) {
+    throw new Error('ID is read-only')
   }
 
-  get graph() {
-    return this._graph;
+  get graph () {
+    return this._graph
   }
 
-  set graph(graph) {
-    if (typeof component === "object") this._graph = graph;
+  set graph (graph) {
+    if (typeof component === 'object') this._graph = graph
     else {
-      throw new Error("Graph is not a valid JSON object");
+      throw new Error('Graph is not a valid JSON object')
     }
   }
 }
 
-module.exports = Graph;
+module.exports = Graph
 
-function resolvePath(string) {
-  if (typeof string !== "string") {
-    return string;
+function resolvePath (string) {
+  if (typeof string !== 'string') {
+    return string
   }
 
-  if (string.charAt(0) !== "@") {
-    return string;
+  if (string.charAt(0) !== '@') {
+    return string
   }
 
-  const [componentId, portId, propertyId] = string.substr(1).split(".");
+  const [componentId, portId, propertyId] = string.substr(1).split('.')
   return {
     componentId,
     portId,
     propertyId
-  };
+  }
 }
